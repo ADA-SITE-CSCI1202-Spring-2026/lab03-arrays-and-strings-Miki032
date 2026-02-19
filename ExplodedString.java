672735456c12a09c5c87b695d4c6ae22b8e515bf
fmikayilov20366 <fmikayilov20366@ada.edu.az>
2026-02-19 09:10:29 +0400

    Add files via upload

---
+++ ExplodedString.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static String explodeString(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            sb.append(str.substring(0, i + 1));
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println(explodeString(input));
+    }
+}
