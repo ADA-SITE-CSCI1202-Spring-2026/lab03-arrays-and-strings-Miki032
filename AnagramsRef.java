89d8794831d2bd30e79aeb6e6636c5b3eb0cb9e4
fmikayilov20366 <fmikayilov20366@ada.edu.az>
2026-02-19 09:34:52 +0400

    Add files via upload

---
+++ AnagramsRef.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+
+    public static boolean areAnagrams(String s1, String s2) {
+        s1 = s1.replaceAll("\\s", "").toLowerCase();
+        s2 = s2.replaceAll("\\s", "").toLowerCase();
+
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+
+        System.out.println(areAnagrams(s1, s2));
+    }
+}
